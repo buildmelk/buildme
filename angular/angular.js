a0d0bcb2882e36a35eb6e0cfae02fea3d85c5d01
sachithra <sewwandisachithra@gmail.com>
2016-01-06 12:37:23 +0530

    index page angular

--- angular/angular.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by hsenid on 12/18/15.
- */
-var app=angular.module('myapp',[])
-
-app.controller('indexCtrl','$scope',function($scope){
-    $scope.service=["Archi","Eng","mss"]
-})
